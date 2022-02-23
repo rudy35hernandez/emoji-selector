2cadf28e4e576d6cd0b7bfc804bd969ab0c961ad
rudy35hernandez <67667227+rudy35hernandez@users.noreply.github.com>
2022-02-23 09:07:08 -0800

    added remove buttons

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 
 
-const inputBtn = document.getElementById("push-btn")
 const inputValue = document.getElementById("emoji-input")
-
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const emojiInput = document.getElementById("emoji-input")
 const emojiContainer = document.getElementById("emoji-container")
-
+const pushBtn = document.getElementById("push-btn")
+const unshiftBtn = document.getElementById("unshift-btn")
+const shiftBtn = document.getElementById("shift-btn")
+const popBtn = document.getElementById("pop-btn")
 
 /// Going to create a function for re usibility when we add new emoji
 
 function renderEmojis(arr){
+    emojiContainer.innerHTML = ""
+
     for (let i = 0; i < arr.length; i++) {
         const emoji = document.createElement('span')
         emoji.textContent = arr[i]
@@ -19,12 +23,31 @@ function renderEmojis(arr){
 
 
 
-inputBtn.addEventListener("click", function(){
-    const emojiInput = document.getElementById("emoji-input");
+pushBtn.addEventListener("click", function(){
     if(emojiInput.value){
         myEmojis.push(emojiInput.value)
         emojiInput.value = ""
-        emojiContainer.innerHTML = ""
         renderEmojis(myEmojis)
     }
+})
+
+
+unshiftBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input");
+    if(emojiInput.value){
+        myEmojis.unshift(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis(myEmojis)
+    }
+})
+
+
+shiftBtn.addEventListener("click", function(){
+    myEmojis.shift()
+    renderEmojis(myEmojis)
+})
+
+popBtn.addEventListener("click", function(){
+    myEmojis.shift()
+    renderEmojis(myEmojis)
 })
